9bca322ae249dfd0b917cd0172521cfca8359ab4
BbqGamer <korba.adam@gmail.com>
2024-10-08 16:31:01 +0200

    Add Solver class and implement Random solver

--- tsp/__init__.py
+++ tsp/__init__.py
@@ -34,3 +34,36 @@ class TSP:
         plt.xlabel("X-axis")
         plt.ylabel("Y-axis")
         plt.show()
+
+    def __len__(self) -> int:
+        return len(self._points)
+
+    def score(self, solution: np.ndarray) -> float:
+        """Return's the score of the solution (distance + weights)"""
+        # weights
+        score = np.sum(self.c[solution])
+
+        # distance
+        closed_path = np.concatenate((solution, [solution[0]]))
+        index_pairs = np.vstack((closed_path[:-1], closed_path[1:])).T
+        score += np.sum(self.D[index_pairs[:, 0], index_pairs[:, 1]])
+
+        return score
+
+
+class Solver(Protocol):
+    def __init__(self, problem: TSP): ...
+    def solve(self) -> np.ndarray:
+        """Solution should be a path of indices, every index should be different
+        We assume that there is an edge between all pairs and between last with first
+        """
+        ...
+
+
+class RandomSolver(Solver):
+    def __init__(self, problem: TSP):
+        self.problem = problem
+
+    def solve(self):
+        path = np.random.choice(len(self.problem), int(np.fix(len(self.problem) / 2)))
+        return path
