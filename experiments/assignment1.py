fb4bbcb8a3162a992c5cf5c5acfe28497830dbb4
BbqGamer <korba.adam@gmail.com>
2024-10-11 19:46:34 +0200

    Add experiment for assignment1

---
+++ experiments/assignment1.py
@@ -0,0 +1,41 @@
+import csv
+import multiprocessing
+
+from tsp import TSP
+from tsp.solvers import GreedyCycle, NNHead, NNWhole, RandomSolver
+
+
+def tsp_task(instance: TSP, filename, solver, starting_node):
+    solution = solver.solve()
+    score = instance.score(solution)
+    row = [
+        filename,
+        solver.__class__.__name__,
+        score,
+        starting_node,
+        str(solution.tolist()).replace(" ", ""),
+    ]
+    return row
+
+
+if __name__ == "__main__":
+    args = []
+    for filename in ["TSPA.csv", "TSPB.csv"]:
+        instance = TSP.from_csv("data/" + filename)
+        for solverc in [RandomSolver, NNHead, NNWhole, GreedyCycle]:
+            for i in range(200):
+                starting_node = i
+                if solverc == RandomSolver:
+                    solver = solverc(instance)
+                    starting_node = -1
+                else:
+                    solver = solverc(instance, starting_node=i)
+                args.append((instance, filename, solver, starting_node))
+
+    with multiprocessing.Pool(16) as p:
+        res = p.starmap(tsp_task, args)
+
+    with open("results/assignment1.csv", "w") as f:
+        writer = csv.writer(f, delimiter=";")
+        for row in res:
+            writer.writerow(row)
