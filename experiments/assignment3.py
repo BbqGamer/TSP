c0759de01bb4769c87a99ba3ad1c2a2d1128a201
BbqGamer <korba.adam@gmail.com>
2024-10-26 16:34:42 +0200

    Add additional metrics for experiment

--- experiments/assignment3.py
+++ experiments/assignment3.py
@@ -1,38 +1,50 @@
 from tsp import TSP, score
 import typing
 from tsp.localsearch import local_search_greedy, local_search_steepest, random_starting, LocalSearchMethod
+import time
 from tsp.localsearch.moves import IntraType
 import numpy as np
-from numba import njit
+from numba import njit, objmode
 
 
 @njit(cache=True)
 def random_start_greedy_experiment(
     n, sol_size, D, intra_move: IntraType, method: LocalSearchMethod
-) -> np.ndarray:
+):
     np.random.seed(42)
 
-    best_score = np.inf
+    scores = []
+    times = []
+    iters = []
     for i in range(200):
         start_sol, unselected = random_starting(n, sol_size)
+        with objmode(start='f8'):
+            start = time.perf_counter()
+
         if method == "steepest":
-            sol = local_search_steepest(start_sol, unselected, D, intra_move)
+            sol, num_iters = local_search_steepest(start_sol, unselected, D, intra_move)
         else:
-            sol = local_search_greedy(start_sol, unselected, D, intra_move)
-        cur_score = score(sol, D)
-        if cur_score < best_score:
-            best_score = cur_score
-            best_sol = sol
-    return best_sol
+            sol, num_iters = local_search_greedy(start_sol, unselected, D, intra_move)
+
+        with objmode(end='f8'):
+            end = time.perf_counter()
+
+        times.append(end - start)
+        scores.append(score(sol, D))
+        iters.append(num_iters)
+    return scores, times, iters
 
 
 if __name__ == "__main__":
-    problem = TSP.from_csv("data/TSPA.csv")
-
-    for intra_move in typing.get_args(IntraType):
-        for method in typing.get_args(LocalSearchMethod):
-            print(intra_move, method)
-            best_sol = random_start_greedy_experiment(
-                len(problem), problem.solution_size, problem.D, intra_move, method
-            )
-            problem.visualize(best_sol)
+    for file in ["data/TSPA.csv", "data/TSPB.csv"]:
+        problem = TSP.from_csv(file)
+        print(f"--- {file} ---")
+        for intra_move in typing.get_args(IntraType):
+            for method in typing.get_args(LocalSearchMethod):
+                print(" ", intra_move, method)
+                scores, times, iters = random_start_greedy_experiment(
+                    len(problem), problem.solution_size, problem.D, intra_move, method
+                )
+                print(f"   score:\t{int(min(scores))}\t{int(sum(scores)/len(scores))}\t{int(max(scores))}")
+                print(f"   times:\t{min(times):.3f}\t{sum(times)/len(times):.3f}\t{max(times):.3f}")
+                print(f"   iters:\t{int(min(iters))}\t{int(sum(iters)/len(iters))}\t{int(max(iters))}")
--- tsp/localsearch/__init__.py
+++ tsp/localsearch/__init__.py
@@ -7,18 +7,22 @@ LocalSearchMethod = Literal["steepest", "greedy"]
 
 
 @njit()
-def local_search_steepest(sol, unselected, D, intra_move: IntraType) -> np.ndarray:
+def local_search_steepest(sol, unselected, D, intra_move: IntraType) -> tuple[np.ndarray, int]:
+    num_iterations = 0
     while True:
         improved = steepest_descent(sol, unselected, D, intra_move)
+        num_iterations += 1
         if not improved:
-            return sol
+            return sol, num_iterations
 
 @njit()
-def local_search_greedy(sol, unselected, D, intra_move: IntraType) -> np.ndarray:
+def local_search_greedy(sol, unselected, D, intra_move: IntraType) ->tuple[np.ndarray, int]:
+    num_iterations = 0
     while True:
         improved = greedy_descent(sol, unselected, D, intra_move)
+        num_iterations += 1
         if not improved:
-            return sol
+            return sol, num_iterations
 
 @njit()
 def random_starting(n, sol_size) -> tuple[np.ndarray, np.ndarray]:
